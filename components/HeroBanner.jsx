8ee32fb530a355958a30ac6051274b1e357d51b4
herom <jerome.manset22@gmail.com>
2023-04-21 02:29:01 +0800

    Create sanity client and fetch data from it

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import Image from 'next/image'
 import Sample from '../public/images/sample.png'
+import Link from 'next/link'
+import { urlFor } from '@/lib/client'
 
-const HeroBanner = () => {
+const HeroBanner = ({heroBanner}) => {
+
+  
+
   return (
     <div className='bg-bannerBG-primary w-11/12 mx-auto mt-4 px-8 py-6 border-2 border-red-500'>
       <div className='md:flex flex-row-reverse border-2 border-yellow-500'>
         <div className='relative flex items-center justify-center basis-1/2 border-2 border-green-500'>
-            <div className='before:absolute before:top-0 before:right-0 before:content-bannerNew'>
+            <div className='before:absolute before:top-0 before:right-0 before:content-bannerNew before:scale-75 md:before:scale-100'>
             </div>
-            <Image src={Sample} className='w-4/6'/>
+            <img src={urlFor(heroBanner.image)} className='w-4/6' alt='atx'/>
         </div>
 
         <div className='basis-1/2 flex flex-col justify-center gap-4 border-2 border-pink-500'>
-            <p className='text-primary-400'>Beats Solo</p>
-            <p className='text-2xl text-primary-400'>Wireless</p>
-            <h1 className='text-6xl text-primary-500'>HEADPHONES</h1>
+            <p className='text-primary-400'>{heroBanner.smallText}</p>
+            <p className='text-2xl text-primary-400'>{heroBanner.midText}</p>
+            <h1 className='text-6xl text-primary-500 uppercase'>{heroBanner.product}</h1>
             <div>
-                <button className='text-white rounded-full py-4 px-8 bg-primary-400'>Shop Wireless Headphones</button>
+              <Link href={`/product/${heroBanner.product}`}>
+                <button type='button' className='text-white rounded-full py-4 px-8 bg-primary-400'>
+                  {heroBanner.buttonText}
+                </button>
+              </Link>
             </div>
         </div>
         
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { HeroBanner, Navbar } from '@/components'
 import Head from 'next/head'
 import React from 'react'
+import {client} from '../lib/client'
 
-const index = () => {
+const index = ({products, bannerData}) => {
   return (
     <>
       <Head>
@@ -11,9 +12,29 @@ const index = () => {
         <meta name='author' content='Jerome Manset'/>
       </Head>
       <Navbar />
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+      
+      <div>
+        <h2>Popular Products</h2>
+        <p>lorem ipsum</p>
+      </div>
+      <div>
+        {products.map(product => product.name)}
+      </div>
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: {products, bannerData}
+  }
 }
 
 export default index
