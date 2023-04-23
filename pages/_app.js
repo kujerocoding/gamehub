023ea5f0bf01c9e6d108cd8052880c121bd87a85
herom <jerome.manset22@gmail.com>
2023-04-23 14:33:46 +0800

    Create StateContext file for state management

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import {Layout} from '@/components'
 import '@/styles/globals.css'
 import React from 'react'
+import { StateContext } from '@/context/StateContext'
 
 export default function App({ Component, pageProps }) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <StateContext>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </StateContext>
   )
 }
